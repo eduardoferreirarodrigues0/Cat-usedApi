a8150300b6214bde75dc96712e1614d579d635f8
Eduardo Ferreira Rodrigues <104681615+eduardoferreirarodrigues0@users.noreply.github.com>
2023-11-13 21:42:25 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const buscarGatinhos = (e) => {
                 const cats = JSON.parse(xhr.responseText)
                 cats.forEach(cat => {
                     const img = document.createElement('img')
+                    const idcat = document.createElement('p')
+                    const hxw = document.createElement('p')
+
                     img.src = cat.url
+                    idcat.textContent = `ID: ${cat.id || 'ID não disponível'}`
+                    hxw.textContent = `Height: ${cat.height  || 'ID não disponível'}`
+
                     document.querySelector("#gatinhos").appendChild(img)
                 })
             } else {
@@ -19,5 +25,38 @@ const buscarGatinhos = (e) => {
     }
     xhr.send()
 }
+
 const btnMostrar = document.querySelector("#mostrar-gatinhos")
 btnMostrar.addEventListener("click", buscarGatinhos)
+
+
+
+//Mostrar Marcas de carros usando FETCH
+
+const getMarcas = () => {
+    const tarefas = fetch('https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json')
+
+    tarefas
+    .then(resposta => resposta.json())
+    .then(marcas => {
+        const ul = document.createElement('ul')
+        marcas.forEach(marca => {
+            const li = document.createElement('li')
+            const logo = document.createElement('img')
+            const nomeMarca = document.createElement('p')
+
+            logo.src = marca.image?.optimized;
+            nomeMarca.textContent = `Marca: ${marca.name || 'Nome não disponível'}`
+
+            li.appendChild(logo)
+            li.appendChild(nomeMarca)
+            ul.appendChild(li)
+            console.log(marca)
+        })
+        document.body.appendChild(ul)
+    })
+    .catch(erro => console.log(erro))
+}
+
+const btnMarcas = document.querySelector("#mostrar-marcas")
+btnMarcas.addEventListener("click",getMarcas)
